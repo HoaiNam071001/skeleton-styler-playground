c77032c7f71d249be3f270e56bc61820f053c2eb
HoaiNam0710 <namnguyen@busmap.vn>
2026-02-24 13:23:17 +0700

    next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  outputFileTracingIncludes: {
+    '/api/types': ['./node_modules/skeleton-styler/dist/**/*'],
+  },
 };
 
 export default nextConfig;
